7393fbc63adf034f3837a6a109f99e561e7d1519
Max Struijs <maxstruys32@gmail.com>
2023-03-30 15:31:27 +0200

    Connection to DB can now be set via config file

--- mysqlscript/mysqlscript.go
+++ mysqlscript/mysqlscript.go
@@ -2,13 +2,40 @@ package mysqlscript
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Config struct {
+	Username     string `json:"username"`
+	Protocol     string `json:"protocol`
+	Host         string `json:"host"`
+	Port         string `json:"port"`
+	DatabaseName string `json:"databaseName"`
+}
+
+func LoadConfiguration(file string) (Config, error) {
+	var config Config
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		return config, err
+	}
+
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&config)
+	return config, err
+}
+
 func InsertToDatabase(result string) {
-	db, err := sql.Open("mysql", "root@tcp(localhost:3306)/test")
+	config, _ := LoadConfiguration("./config.json")
+
+	//db, err := sql.Open("mysql", "root@tcp(localhost:3306)/test")
+	var connection string = "" + config.Username + "@" + config.Protocol + "(" + config.Host + ":" + config.Port + ")" + config.DatabaseName + ""
+	db, err := sql.Open("mysql", connection)
 	if err != nil {
 		fmt.Println("Cannot find MSQL database!")
 	}
